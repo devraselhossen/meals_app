9f931f39e0ecd9d808d45d89c7a4c467d5b5e76e
rasel-hossen <straselhossen24@gmail.com>
2022-04-13 15:35:03 +0600

    first

---
+++ lib/dummy_data.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: constant_identifier_names, unnecessary_const
+
+import 'package:flutter/material.dart';
+import 'package:meals_app/model/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: "c1", title: "Break-fast", color: Colors.purple),
+  Category(id: "c2", title: "Break-fast", color: Colors.black),
+  Category(id: "c3", title: "Break-fast", color: Colors.pink),
+  Category(id: "c4", title: "Break-fast", color: Colors.green),
+  Category(id: "c5", title: "Break-fast", color: Colors.blue),
+  Category(id: "c6", title: "Break-fast", color: Colors.red),
+  Category(id: "c7", title: "Break-fast", color: Colors.yellow),
+  Category(id: "c8", title: "Break-fast", color: Colors.amber),
+  Category(id: "c9", title: "Break-fast", color: Colors.brown),
+  Category(id: "c10", title: "Break-fast", color: Colors.deepOrange),
+];
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "DailiMeals",
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      appBar: AppBar(
+        title: Text("DealiMeals"),
+      ),
+      
+    );
+  }
+}
