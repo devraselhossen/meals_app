3373f1b4fe3de203cbcdcc2c79ee77d4184e2958
rasel-hossen <straselhossen24@gmail.com>
2022-05-19 11:51:07 +0600

    filter change

--- lib/screen/filter_screen.dart
+++ lib/screen/filter_screen.dart
@@ -4,18 +4,127 @@ import 'package:flutter/material.dart';
 
 import '../widget/main_drawer.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  bool _glutenFree = false;
+  bool _lactoseFree = false;
+  bool _vegan = false;
+  bool _vegetarian = false;
+
+  // Widget buildSwitchListTile(
+  //     String title, String subTitle, bool currentValue, Function updateValue) {
+  //   return SwitchListTile(
+  //     title: Text(title),
+  //     value: currentValue,
+  //     subtitle: Text(subTitle),
+  //     onChanged: updateValue(),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Your Filters")
-      ),
+      appBar: AppBar(title: Text("Your Filters")),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text("Filters"),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text("Adjust your meal section.",
+                style: Theme.of(context).textTheme.titleLarge),
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              // buildSwitchListTile(
+              //   "Gluten-free",
+              //   "Only include gluten-free meals",
+              //   _glutenFree,
+              //   (newValue) {
+              //     setState(() {
+              //       _glutenFree = newValue;
+              //     });
+              //   },
+              // ),
+              // buildSwitchListTile(
+              //   "Lactose-free",
+              //   "Only include lactose-free meals",
+              //   _lactoseFree,
+              //   (newValue) {
+              //     setState(() {
+              //       _lactoseFree = newValue;
+              //     });
+              //   },
+              // ),
+              // buildSwitchListTile(
+              //   "Vegetarian",
+              //   "Only include vegetarian meals",
+              //   _vegetarian,
+              //   (newValue) {
+              //     setState(() {
+              //       _vegetarian = newValue;
+              //     });
+              //   },
+              // ),
+              // buildSwitchListTile(
+              //   "Vegan",
+              //   "Only include vegan meals",
+              //   _vegan,
+              //   (newValue) {
+              //     setState(() {
+              //       _vegan = newValue;
+              //     });
+              //   },
+              // ),
+              SwitchListTile(
+                title: Text("Gluten-free"),
+                value: _glutenFree,
+                subtitle: Text("Only include gluten-free meals"),
+                onChanged: (newValue) {
+                  setState(() {
+                    _glutenFree = newValue;
+                  });
+                },
+              ),
+              SwitchListTile(
+                title: Text("Lactose-free"),
+                value: _lactoseFree,
+                subtitle: Text("Only include lactose-free meals"),
+                onChanged: (newValue) {
+                  setState(() {
+                    _lactoseFree = newValue;
+                  });
+                },
+              ),
+              SwitchListTile(
+                title: Text("Vegetarian"),
+                value: _vegetarian,
+                subtitle: Text("Only include vegetarian meals"),
+                onChanged: (newValue) {
+                  setState(() {
+                    _vegetarian = newValue;
+                  });
+                },
+              ),
+              SwitchListTile(
+                title: Text("Vegan"),
+                value: _vegan,
+                subtitle: Text("Only include vegan meals"),
+                onChanged: (newValue) {
+                  setState(() {
+                    _vegan = newValue;
+                  });
+                },
+              ),
+            ],
+          )),
+        ],
       ),
     );
   }
